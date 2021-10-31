e266d125c5354f17cbe576f13b58b8c52e33fa2a
MarcusDunn <marcus.s.dunn@gmail.com>
2021-10-30 19:19:58 -0700

    added test to guarantee client was generated

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import {Service} from '../dist'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+test('client was generated prior', () => {
+    expect(Service.getPing()).not.toBeUndefined()
+})
