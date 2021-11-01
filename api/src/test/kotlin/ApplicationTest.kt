7243c25bea935336cffe9743c2da2724d1da63cd
MarcusDunn <marcus.s.dunn@gmail.com>
2021-10-31 19:54:59 -0700

    added editor config

--- api/src/test/kotlin/ApplicationTest.kt
+++ api/src/test/kotlin/ApplicationTest.kt
@@ -1,15 +1,4 @@
-import ca.vikelabs.maps.application
-import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
-import org.http4k.core.ContentType
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Status
-import org.http4k.format.Jackson
-import org.http4k.hamkrest.hasContentType
-import org.http4k.hamkrest.hasStatus
 import org.junit.jupiter.api.Test
-import java.io.FileNotFoundException
 import kotlin.test.assertTrue
 
 class ApplicationTest {
