b8ddfccb393124414040e0a26ef7a3a4da157dab
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-15 21:08:18 -0800

    cut the chaff

--- api/src/main/kotlin/ca/vikelabs/maps/routes/MapsResponse.kt
+++
@@ -1,9 +0,0 @@
-package ca.vikelabs.maps.routes
-
-/**
- * base interface for every response maps returns
- */
-sealed class MapsResponse(val success: Boolean) {
-    open class Success : MapsResponse(success = true)
-    open class Failure(open val reason: String) : MapsResponse(success = false)
-}
--- api/src/main/kotlin/ca/vikelabs/maps/routes/Ping.kt
+++ api/src/main/kotlin/ca/vikelabs/maps/routes/Ping.kt
@@ -5,17 +5,19 @@ import org.http4k.contract.meta
 import org.http4k.core.*
 import org.http4k.format.Jackson.auto
 
+data class Success(val success: Boolean = true)
+
 fun ping(): ContractRoute {
-    val body = Body.auto<MapsResponse.Success>().toLens()
+    val body = Body.auto<Success>().toLens()
 
     val spec = "/ping" meta {
         summary = "Returns 200 OK and a small json object describing the server status."
         description = "A handy endpoint for checking to see weather the server is alive and replying."
-        returning(Status.OK, body to MapsResponse.Success())
+        returning(Status.OK, body to Success())
     } bindContract Method.GET
 
     val ping: HttpHandler = { _ ->
-        Response(Status.OK).with(body of MapsResponse.Success())
+        Response(Status.OK).with(body of Success())
     }
 
     return spec to ping
