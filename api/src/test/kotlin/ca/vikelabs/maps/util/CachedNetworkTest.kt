d3570a1de5a50ca4bbf17e6a73b85d36b38ad868
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-20 17:08:35 -0800

    added forgotten file

---
+++ api/src/test/kotlin/ca/vikelabs/maps/util/CachedNetworkTest.kt
@@ -0,0 +1,24 @@
+package ca.vikelabs.maps.util
+
+import mu.KotlinLogging
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.HttpHandler
+import org.http4k.core.then
+import org.http4k.filter.TrafficFilters
+import org.http4k.traffic.ReadWriteCache
+
+private val logger = KotlinLogging.logger { }
+
+abstract class CachedNetworkTest(
+    private val networkClient: HttpHandler? = JavaHttpClient(),
+    storage: ReadWriteCache = ReadWriteCache.Disk()
+) {
+
+    val cachedClient = TrafficFilters.ServeCachedFrom(storage)
+        .then(TrafficFilters.RecordTo(storage))
+        .then { request ->
+            logger.warn { "a CachedNetworkTest is falling back onto a network call. This should only occur on the first round of testing." }
+            networkClient?.invoke(request)
+                ?: throw Exception("no network client to fall back on")
+        }
+}
