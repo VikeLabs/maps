59aedefabcaec8b491c075794e3480bf3edab609
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-22 15:52:32 -0800

    explict named params on openapi config

--- api/src/main/kotlin/ca/vikelabs/maps/Application.kt
+++ api/src/main/kotlin/ca/vikelabs/maps/Application.kt
@@ -22,8 +22,12 @@ fun application(
     config: Config = warnAndDefault
 ): HttpHandler = contract {
     renderer = OpenApi3(
-        apiInfo = ApiInfo("map uvic", "0.0.1", "An API for navigating around the University of Victoria."),
-        Jackson
+        apiInfo = ApiInfo(
+            title = "map uvic",
+            version = "0.0.1",
+            description = "An API for navigating around the University of Victoria."
+        ),
+        json = Jackson,
     )
     descriptionPath = "/"
     routes += ping()
