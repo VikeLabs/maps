bedff4f0874724d3c68b6d210cbf210dc776fdef
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-22 16:21:21 -0800

    added uvic as starting location

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
     import * as L from 'leaflet';
     import 'leaflet/dist/leaflet.css';
+
     let map: L.Map;
 
     const createMap = (container: HTMLElement) => {
-        let m = L.map(container).setView([51.505, -0.09], 13);
+        let m = L.map(container).setView([48.463069, -123.311833], 16);
         L.tileLayer(
             'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
             {
                 attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
           &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
                 subdomains: 'abcd',
-                maxZoom: 14,
+                maxZoom: 20,
             }
         ).addTo(m);
 
