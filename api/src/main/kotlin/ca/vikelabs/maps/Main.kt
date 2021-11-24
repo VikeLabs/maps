efc70a3d9a47a21ed5c01b18fde05b5e3f3a7173
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-24 14:23:08 -0800

    added search to frontend

--- api/src/main/kotlin/ca/vikelabs/maps/Main.kt
+++ api/src/main/kotlin/ca/vikelabs/maps/Main.kt
@@ -1,6 +1,9 @@
 package ca.vikelabs.maps
 
 import mu.KotlinLogging
+import org.http4k.core.then
+import org.http4k.filter.CorsPolicy
+import org.http4k.filter.ServerFilters
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
@@ -8,7 +11,8 @@ private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
     val configuration = Config.fromArgs(args)
-    application(configuration)
+    ServerFilters.Cors(CorsPolicy.UnsafeGlobalPermissive)
+        .then(application(configuration))
         .asServer(Jetty(configuration.port))
         .start()
         .also { logger.info { "Maps server started! Running on: http://localhost:${it.port()}" } }
--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import * as L from 'leaflet';
     import 'leaflet/dist/leaflet.css';
+    import SearchBar from "./SearchBar.svelte";
+    import {Service} from "../dist";
 
     let map: L.Map;
 
     function mapAction(container: HTMLElement) {
-
-        map = L.map(container).setView([48.463069, -123.311833], 16);
+        let uvicCoords = [48.463069, -123.311833];
+        map = L.map(container).setView(uvicCoords, 16);
 
         L.tileLayer(
             'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
@@ -16,13 +18,45 @@
                 maxZoom: 20,
             }
         ).addTo(map);
+        toolbar.addTo(map);
 
         return {
-            destroy: () => {
-                map.remove();
-            },
+            destroy: map.remove,
         };
     }
+
+    let toolbar = L.control({position: 'topright'});
+
+    let toolbarComponent;
+
+    toolbar.onAdd = (map) => {
+        let div = L.DomUtil.create('div');
+        toolbarComponent = new SearchBar({
+            target: div,
+            props: {}
+        });
+
+        toolbarComponent.$on('search', async ({detail: text}) => {
+            let resp = await Service.getSearch(text)
+            for (let i = 0; i < resp.results.length; i++) {
+                const building = resp.results[i];
+                // opacity is set to zero because we don't currently bundle assets.
+                L.marker([building.center.latitude, building.center.longitude], {opacity: 0})
+                    .bindTooltip(building.name)
+                    .addTo(map) // you have to add to map before opening
+                    .openTooltip()
+            }
+        })
+
+        return div;
+    }
+
+    toolbar.onRemove = () => {
+        if (toolbarComponent) {
+            toolbarComponent = null;
+        }
+    };
+
 </script>
 
 <div style="height:100vh;width:100vw" use:mapAction></div>
