6200a11d77c5d97181c41f9dfd860e9d387e74bc
Magdalen Thot <43592505+magdalent@users.noreply.github.com>
2022-03-17 17:37:59 -0700

    search bar overflow

--- app/src/lib/MapSearch.svelte
+++ app/src/lib/MapSearch.svelte
@@ -13,3 +13,21 @@
         dispatch(Events.Search, {text})
     }
 }}>
+
+<style>
+    input{
+        margin-left: 2rem;
+        padding-left: 1rem;
+        border-radius: .5rem;
+        height: 1.9rem;
+        width: 15rem;  
+        font-size: 0.9rem;
+        border: 2px solid rgb(0, 0, 0);
+        box-shadow: 0 4px 3px rgba(126, 126, 126, 0.2);
+    }
+
+    input:active{
+        border: 2.6px solid rgb(70, 139, 243);
+    }
+
+</style>
